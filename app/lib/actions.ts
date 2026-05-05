a653a9a3b7d7a06e542f3a5b67a8eaafc922f573
Mateus Lopes <mateusbslopes@gmail.com>
2026-05-05 12:02:59 -0300

    feat: chapter 13 add validation to invoice edit

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -68,12 +68,21 @@ export async function createInvoice(_prevState: State, formData: FormData) {
 
 const UpdateInvoice = FormSchema.omit({ id: true, date: true });
 
-export async function updateInvoice(id: string, formData: FormData) {
-    const { customerId, amount, status } = UpdateInvoice.parse({
+export async function updateInvoice(id: string, _prevState: State, formData: FormData) {
+    const validatedFields = UpdateInvoice.safeParse({
         customerId: formData.get('customerId'),
         amount: formData.get('amount'),
         status: formData.get('status'),
     });
+
+    if (!validatedFields.success) {
+        return {
+            errors: validatedFields.error.flatten().fieldErrors,
+            message: 'Missing Fields. Failed to Create Invoice.',
+        };
+    }
+
+    const { customerId, amount, status } = validatedFields.data;
     const amountInCents = amount * 100;
 
     try {
@@ -83,8 +92,6 @@ export async function updateInvoice(id: string, formData: FormData) {
             WHERE id = ${id}
         `;
     } catch (error) {
-        // We'll also log the error to the console for now
-        console.error(error);
         return { message: 'Database Error: Failed to Update Invoice.' };
     }
     revalidatePath('/dashboard/invoices');
